4d1eb73523c021d5c047fe7abd0194b04c0b3382
Michaela202 <veselovskamichaela222@gmail.com>
2022-11-21 14:12:01 +0100

    testbranch

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,7 @@ public class Test
     System.out.println("Test");
     System.out.println("Test2");
     System.out.println("Testasd");
-    System.out.println("testfadfuahrigahbranch");
+
     System.out.println("Michaela");
     System.out.println("Bence");
     System.out.println("Igor");
