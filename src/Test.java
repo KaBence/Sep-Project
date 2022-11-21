21c8befe01c0a769eb786a2e1aadd61e89f32233
KaBence <kabaly.bence@gmail.com>
2022-11-21 14:50:00 +0100

    test merging from pull request

--- src/Test.java
+++ src/Test.java
@@ -5,13 +5,7 @@ public class Test
   public static void main(String[] args)
   {
     Scanner k=new Scanner(System.in);
-    System.out.println("Test");
-    System.out.println("Test2");
-
-
-    System.out.println("Michaela");
-    System.out.println("Bence");
-    System.out.println("Igor");
+    System.out.println("Test merging from pull request");
   }
 
 }
