5175c64afd0703ba8adb9a7905124d47bee48704
KaBence <kabaly.bence@gmail.com>
2022-11-21 14:14:23 +0100

    scanner

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 
 public class Test
 {
   public static void main(String[] args)
   {
+    Scanner k=new Scanner(System.in);
     System.out.println("Test");
     System.out.println("Test2");
     System.out.println("Testasd");
