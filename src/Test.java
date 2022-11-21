fb8a81fc96b85debff62050c3119dbc07ddeaf93
Michaela202 <veselovskamichaela222@gmail.com>
2022-11-21 14:16:19 +0100

    testbranch

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,6 @@ public class Test
     Scanner k=new Scanner(System.in);
     System.out.println("Test");
     System.out.println("Test2");
-    System.out.println("Testasd");
 
 
     System.out.println("Michaela");
