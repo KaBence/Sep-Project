a5e875a18301c6dc917c372dbabdd14f74fe0ef7
KaBence <kabaly.bence@gmail.com>
2022-11-28 17:26:16 +0100

    Finished BoardGameList

--- src/Model/BoardGameList.java
+++ src/Model/BoardGameList.java
@@ -17,5 +17,45 @@ public class BoardGameList
      boardGames.remove(boardGame);
    }
 
+  public BoardGameList getBoardGamesByType(String type){
+     BoardGameList temp=new BoardGameList();
+     for (BoardGame item: boardGames){
+       if (item.getType().equals(type)){
+         temp.addBoardGame(item);
+       }
+     }
+     return temp;
+  }
 
+  public BoardGameList getBoardGamesByNoP(int num){
+     BoardGameList temp=new BoardGameList();
+     for (BoardGame item: boardGames){
+       if (item.getMaxNoP()>num&&item.getMinNoP()<num)temp.addBoardGame(item);
+     }
+     return temp;
+  }
+
+  public BoardGameList getBoardGamesByAvailability(boolean availability){
+     BoardGameList temp=new BoardGameList();
+     for (BoardGame item:boardGames){
+       if (item.isAvailable()) temp.addBoardGame(item);
+     }
+     return temp;
+  }
+
+  public BoardGameList getBoardGamesByOwner(Member owner){
+    BoardGameList temp=new BoardGameList();
+    for (BoardGame item:boardGames){
+      if (item.getOwner().equals(owner)) temp.addBoardGame(item);
+    }
+    return temp;
+  }
+
+  public BoardGameList getBoardGamesByName(String name){
+    BoardGameList temp=new BoardGameList();
+    for (BoardGame item:boardGames){
+      if (item.getName().equals(name)) temp.addBoardGame(item);
+    }
+    return temp;
+  }
 }
