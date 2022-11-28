74279b6657eaefa4c95d14c7fb97ddd0c40783a2
KaBence <kabaly.bence@gmail.com>
2022-11-28 16:07:31 +0100

    Done the BoardGame and half of the BoardGameList

--- src/Model/BoardGame.java
+++ src/Model/BoardGame.java
@@ -23,4 +23,66 @@ public class BoardGame
     rankList=new RankList();
   }
 
+  public Member getOwner(){
+    return owner;
+  }
+  public void setOwner(Member owner)
+  {
+    this.owner = owner;
+  }
+  public int getMaxNoP()
+  {
+    return maxNoP;
+  }
+  public int getMinNoP()
+  {
+    return minNoP;
+  }
+  public void setMaxNoP(int maxNoP) {
+    this.maxNoP = maxNoP;
+  }
+  public void setMinNoP(int minNoP) {
+    this.minNoP = minNoP;
+  }
+  public String getName()
+  {
+    return name;
+  }
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  public void setType(String type)
+  {
+    this.type = type;
+  }
+  public String getType()
+  {
+    return type;
+  }
+  public void setAvailability(boolean availability) {
+    this.availability = availability;
+  }
+  public boolean isAvailable()
+  {
+    return availability;
+  }
+  public boolean isBorrowed(){
+    return borrow!=null;
+  }
+  public boolean isReserved(){
+    return reservationList!=null;
+  }
+
+
+
+  public boolean equals(Object obj){
+    if (obj==null||getClass()!=obj.getClass()) return false;
+    BoardGame temp=(BoardGame) obj;
+    return type.equals(temp.type)&&name.equals(temp.name)&&availability==temp.availability&&minNoP==temp.minNoP&&maxNoP==temp.maxNoP&&owner.equals(temp.owner)&&reservationList.equals(temp.reservationList)&&borrow.equals(temp.borrow)&&rankList.equals(temp.rankList)&&voteList.equals(temp.voteList);
+  }
+//toString is not done because we don't know if we need it
+  public String toString(){
+    return name+" "+type+" "+minNoP+"-"+maxNoP+" "+owner;
+  }
 }
--- src/Model/BoardGameList.java
+++ src/Model/BoardGameList.java
@@ -1,5 +1,23 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class BoardGameList
 {
+   private ArrayList<BoardGame> boardGames;
+   public BoardGameList(){
+     boardGames=new ArrayList<>();
+   }
+
+   public void addBoardGame(BoardGame boardGame){
+     boardGames.add(boardGame);
+   }
+
+   public void removeBoardGame(BoardGame boardGame){
+     boardGames.remove(boardGame);
+   }
+
+   public void editBoardGame(int index,BoardGame boardGame){
+
+   }
 }
