1472ee63548bf5e1e614265e36dc2d0116a0d7dd
KaBence <kabaly.bence@gmail.com>
2022-11-29 14:17:58 +0100

    Did the loadviews but is not working yet

--- src/GUI/MenuController.java
+++ src/GUI/MenuController.java
@@ -9,15 +9,24 @@ import javafx.scene.control.Button;
 public class MenuController
 {
   private ViewHandler viewHandler;
-  private BoardGameClub clubmanager;
+  private BoardGameClub clubManager;
   private Scene scene;
 
   @FXML Button addBoardGame;
+  @FXML Button manageBoardGame;
+  @FXML Button addMember;
+  @FXML Button manageMember;
+  @FXML Button addEvent;
+  @FXML Button manageEvent;
+  @FXML Button vote;
+  @FXML Button reservation;
+  @FXML Button borrow;
+  @FXML Button returnGame;
   public void init(ViewHandler viewHandler, Scene scene, BoardGameClub clubManager)
   {
     this.viewHandler = viewHandler;
     this.scene = scene;
-    this.clubmanager = clubmanager;
+    this.clubManager = clubManager;
   }
 
   public Scene getScene(){
@@ -26,5 +35,14 @@ public class MenuController
 
   public void actionHandler(ActionEvent e){
     if (e.getSource()==addBoardGame) viewHandler.openView("addBoardGame");
+    if (e.getSource()==manageBoardGame) viewHandler.openView("manageBoardGame");
+    if (e.getSource()==addEvent) viewHandler.openView("addEvent");
+    if (e.getSource()==manageEvent) viewHandler.openView("manageEvent");
+    if (e.getSource()==addMember) viewHandler.openView("addMember");
+    if (e.getSource()==manageMember) viewHandler.openView("manageMember");
+    if (e.getSource()==vote) viewHandler.openView("vote");
+    if (e.getSource()==reservation) viewHandler.openView("reservation");
+    if (e.getSource()==borrow) viewHandler.openView("borrow");
+    if (e.getSource()==returnGame) viewHandler.openView("returnGame");
   }
 }
