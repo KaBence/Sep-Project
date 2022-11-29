1da2f029e038fdad5d0d788ce52bbf2a527b36fb
Michaela202 <veselovskamichaela222@gmail.com>
2022-11-29 17:19:42 +0100

    resizing

--- src/GUI/ManageBoardGamesController.java
+++ src/GUI/ManageBoardGamesController.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
@@ -13,27 +13,64 @@ import java.io.IOException;
 
 public class ManageBoardGamesController
 {
+  @FXML RadioButton name, type, numOfPlayers;
+  @FXML RadioButton available, reserved, borrowed, allGames;
+  @FXML TextField searchField;
+  @FXML Button search;
+  @FXML TableView games;
+  @FXML Button back, temporary;
+  @FXML TextField nameOfGame;
+  @FXML TextField min;
+  @FXML TextField max;
+  @FXML TextField typeOfBoardGame;
+  @FXML ComboBox owner;
+  @FXML RadioButton available2, nonAvailable2, reserved2, borrowed2;
+  @FXML Button edit;
+  @FXML Button remove;
+  @FXML Button seeReviews;
+  @FXML Button reserve;
+  @FXML Button borrow;
+  @FXML Button save3;
+  @FXML ComboBox borrower3;
+  @FXML DatePicker pickupdate3;
+  @FXML DatePicker returnDate3;
+  @FXML TableView reservations3;
+  @FXML Button home;
+  @FXML Button edit3;
+  @FXML Button remove3;
+  @FXML TextField name4;
+  @FXML TextField rank;
+  @FXML ListView reviews;
+
+
+
+
+
+
 
   private ViewHandler viewHandler;
   private BoardGameClub clubManager;
   private Scene scene;
 
-  @FXML Button back,temporary;
-
-  public void init(ViewHandler viewHandler, Scene scene, BoardGameClub clubManager)
+  public void init(ViewHandler viewHandler, Scene scene,
+      BoardGameClub clubManager)
   {
     this.viewHandler = viewHandler;
     this.scene = scene;
     this.clubManager = clubManager;
   }
 
-  public Scene getScene(){
+  public Scene getScene()
+  {
     return scene;
   }
 
-  public void actionHandler(ActionEvent e){
-    if (e.getSource()==back) viewHandler.start();
-    if (e.getSource()==temporary){
+  public void actionHandler(ActionEvent e)
+  {
+    if (e.getSource() == back)
+      viewHandler.start();
+    if (e.getSource() == temporary)
+    {
       try
       {
         FXMLLoader loader = new FXMLLoader();
@@ -45,7 +82,7 @@ public class ManageBoardGamesController
       {
         ed.printStackTrace();
       }
-      Stage temp=new Stage();
+      Stage temp = new Stage();
       temp.setTitle("Test");
       temp.setScene(getScene());
       temp.show();
