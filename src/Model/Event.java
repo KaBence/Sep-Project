a8bac7af2d86c4f683d99d232e40ca4fb78d5110
igorcretu <igorcretsu09@gmail.com>
2022-11-30 01:17:09 +0100

    Events, Reservations and fxml stuff

--- src/Model/Event.java
+++ src/Model/Event.java
@@ -1,5 +1,86 @@
 package Model;
+import java.util.ArrayList;
+public class Event {
+    private int time;
+    private String location;
+    private String name;
+    private ArrayList<String> guests;
+    private MyDate date;
+    private BoardGame game;
 
-public class Event
-{
+    public Event(int time, String location, String name, ArrayList<String> guests){
+        this.time = time;
+        this.location = location;
+        this.name = name;
+        this.guests = guests;
+    }
+
+    public int getTime(){
+        return time;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setDate(MyDate date){
+        this.date = date;
+    }
+
+    public String toString(){
+        return name + " on " +date.toString()+" "+ time + "at" + location;
+    }
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setGuests(ArrayList<String> guests) {
+        this.guests = guests;
+    }
+    public boolean equals(Object obj){
+
+        if (obj == null || getClass() != obj.getClass())
+        {
+            return false;
+        }
+        Event other = (Event)obj;
+        return time == other.time &&
+                location.equals(other.location) &&
+                name.equals(other.name);
+    }
+    public void setBoardGame(BoardGame boardGame){
+        this.game = boardGame;
+    }
+
+    public MyDate getDate() {
+        return date;
+    }
+    public Event copy(){
+        return new Event(time,location,name,guests);
+    }
+
+    public BoardGame getGame() {
+        return game;
+    }
+    public String getLocation(){
+        return location;
+    }
+
+    public void addGuest(String guest) {
+        guests.add(guest);
+    }
+    public void RemoveGuest(String name){
+        guests.remove(name);
+
+//        for (int i = 0; i<guests.size();i++){
+//            if (name.equals(guests.get(i))) {guests.remove(i); break;}
+//        }
+    }
 }
+
--- src/Model/ReservationList.java
+++ src/Model/ReservationList.java
@@ -1,5 +1,25 @@
 package Model;
-
+import java.util.ArrayList;
 public class ReservationList
 {
+    private ArrayList<Reservation> list;
+
+    public ReservationList(){
+    }
+    public void addReservation(Reservation reservation){
+        list.add(reservation);
+    }
+    public void setReservations(ArrayList<Reservation> reservations){
+        list = reservations;
+    }
+    public ArrayList<Reservation> getReservations(){
+        return list;
+    }
+    public String toString(){
+        String a = "";
+        for (int i = 0; i<list.size();i++){
+            a += list.get(i).toString() +"\n";
+        }
+        return a;
+    }
 }
