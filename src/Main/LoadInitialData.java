7c28222c3168b7af8d4b1b6233e1d2a545d23d90
KaBence <kabaly.bence@gmail.com>
2022-12-01 10:54:31 +0100

    Merged the members and the boardgame load initial data

--- src/Main/LoadInitialData.java
+++ src/Main/LoadInitialData.java
@@ -22,26 +22,6 @@ public class LoadInitialData
     BoardGameList boardgames=new BoardGameList();
     MemberList members=new MemberList();
     String[] boardgamesarray=null;
-    try
-    {
-      boardgamesarray= MyFileHandler.readArrayFromTextFile("dummydataboardgames.txt");
-
-      for (String item:boardgamesarray){
-        String[] temp=item.split(",");
-        String name=temp[0];
-        String type=temp[1];
-        int minNoP=Integer.parseInt(temp[2]);
-        int maxNop=Integer.parseInt(temp[3]);
-        String owner=temp[4];
-        boolean avl= Boolean.parseBoolean(temp[5]);
-        boardgames.addBoardGame(new BoardGame(name,type,minNoP,maxNop,
-            members.getMemberByName(owner),avl));
-      }
-    }
-    catch (FileNotFoundException e){
-      System.out.println("File not found");
-    }
-    MemberList members = new MemberList();
     String[] memberArray = null;
 
     try
@@ -67,6 +47,27 @@ public class LoadInitialData
 
     try
     {
+      boardgamesarray= MyFileHandler.readArrayFromTextFile("dummydataboardgames.txt");
+
+      for (String item:boardgamesarray){
+        String[] temp=item.split(",");
+        String name=temp[0];
+        String type=temp[1];
+        int minNoP=Integer.parseInt(temp[2]);
+        int maxNop=Integer.parseInt(temp[3]);
+        String owner=temp[4];
+        boolean avl= Boolean.parseBoolean(temp[5]);
+        boardgames.addBoardGame(new BoardGame(name,type,minNoP,maxNop,
+            members.getMemberByName(owner),avl));
+      }
+    }
+    catch (FileNotFoundException e){
+      System.out.println("File not found");
+    }
+
+
+    try
+    {
       MyFileHandler.writeToBinaryFile("members.bin", members);
     }
     catch (FileNotFoundException e)
@@ -79,6 +80,7 @@ public class LoadInitialData
     }
 
     System.out.println("Done");
+
     try
     {
       MyFileHandler.writeToBinaryFile("Boardgames.bin",boardgames);
