84ffd48f29dd486add87efd7e40cf95f067af10a
igorcretu <igorcretsu09@gmail.com>
2022-12-09 06:38:30 +0100

    a lot of alerts added

--- src/Model/Event.java
+++ src/Model/Event.java
@@ -1,8 +1,9 @@
 package Model;
+
 import java.io.Serializable;
 import java.util.ArrayList;
-public class Event implements Serializable
-{
+
+public class Event implements Serializable {
     private String location;
     private String name;
     private ArrayList<String> guests;
@@ -13,7 +14,7 @@ public class Event implements Serializable
     private int capacity;
     private MemberList members;
 
-    public Event(MyDate date, String location, String name, String guests, int capacity, BoardGameList games, MemberList members){
+    public Event(MyDate date, String location, String name, String guests, int capacity, BoardGameList games, MemberList members) {
 
         this.location = location;
         this.name = name;
@@ -22,7 +23,7 @@ public class Event implements Serializable
         guestsString = guests;
         ArrayList<String> a = new ArrayList<String>();
         String[] tempArr = guests.split(",");
-        for (int i = 0; i<tempArr.length; i++){
+        for (int i = 0; i < tempArr.length; i++) {
             a.add(tempArr[i]);
         }
         this.guests = a;
@@ -30,25 +31,27 @@ public class Event implements Serializable
         this.members = members;
     }
 
-        public void setCapacity(int maxCapacity)
-    {
+    public void setCapacity(int maxCapacity) {
         this.maxCapacity = maxCapacity;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public int getCapacity(){
+
+    public int getCapacity() {
         return capacity;
     }
-    public void setDate(MyDate date){
+
+    public void setDate(MyDate date) {
         this.date = date;
     }
 
-    public String toString(){
-        return name + " on " +date+ " at " + location +" with a max capcity of "+capacity +" players, playing these games" + games.toString()+"with these members: " +members+" and having these guests: \n" + guests;
+    public String toString() {
+        return name + " on " + date + " at " + location + " with a max capcity of " + capacity + " players, playing these games" + games.toString() + "with these members: " + members + " and having these guests: \n" + guests.toString().replace("[", "").replace("]", "");
     }
-    public void setLocation(String location){
+
+    public void setLocation(String location) {
         this.location = location;
     }
 
@@ -56,61 +59,76 @@ public class Event implements Serializable
         this.name = name;
     }
 
-    public boolean equals(Object obj){
+    public boolean equals(Object obj) {
 
-        if (obj == null || getClass() != obj.getClass())
-        {
+        if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
-        Event other = (Event)obj;
+        Event other = (Event) obj;
         return
                 location.equals(other.location) &&
                         name.equals(other.name) && guests.equals(other.guests)
-                && date.equals(other.date) && capacity == other.capacity;
+                        && date.equals(other.date) && capacity == other.capacity &&
+                        games.equals(other.games) && members.equals(other.members);
     }
 
     public MyDate getDate() {
         return date;
     }
-    public Event copy(){
+    public MyDate getDateNoTime(){
+        return new MyDate(date.getDay(), date.getMonth(), date.getYear()) ;
+    }
+
+    public Event copy() {
         String a = "";
-        for (int i = 0; i< guests.size();i++){
-            a+= guests.get(i);
+        for (int i = 0; i < guests.size(); i++) {
+            a += guests.get(i);
         }
-        return new Event(date,location,name, a,capacity,games, members);
+        return new Event(date, location, name, a, capacity, games, members);
     }
 
     public BoardGameList getGames() {
         return games;
     }
-    public MemberList getMembers(){
+
+    public MemberList getMembers() {
         return members;
     }
-    public void setMembers(MemberList list){
+
+    public void setMembers(MemberList list) {
         members = list;
     }
 
-    public void addGames(BoardGame boardGame){
+    public void addGames(BoardGame boardGame) {
         games.addBoardGame(boardGame);
     }
-    public void setGames(BoardGameList games){
+
+    public void setGames(BoardGameList games) {
         this.games = games;
     }
-    public String getLocation(){
+
+    public String getLocation() {
         return location;
     }
-    public String getGuests(){
+
+    public String getGuests() {
         return guestsString;
     }
-    public ArrayList<String> getGuestsArr(){
+
+    public ArrayList<String> getGuestsArr() {
         return guests;
     }
+
     public void addGuest(String guest) {
         guests.add(guest);
     }
-    public void RemoveGuest(String name){
-        for (int i = 0; i<guests.size();i++){
-            if (name.equals(guests.get(i))) {guests.remove(i); break;}
+
+    public void RemoveGuest(String name) {
+        for (int i = 0; i < guests.size(); i++) {
+            if (name.equals(guests.get(i))) {
+                guests.remove(i);
+                break;
+            }
         }
     }
 }
